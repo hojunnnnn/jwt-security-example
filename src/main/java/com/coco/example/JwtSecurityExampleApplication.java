319688c3afe943ec47e0b6fb578eb8ab7b4b0d46
이호준 <59757689+hojunnnnn@users.noreply.github.com>
2022-04-16 12:06:48 +0900

    Initial commit

---
+++ src/main/java/com/coco/example/JwtSecurityExampleApplication.java
@@ -0,0 +1,13 @@
+package com.coco.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtSecurityExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtSecurityExampleApplication.class, args);
+    }
+
+}
